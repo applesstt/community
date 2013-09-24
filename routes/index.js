b8149f3e0db41535836bc0ee3875c69ddba5405a
liyachuan <liyachuan@rockontrol.com>
2013-09-24 22:39:03 +0800

    init routes on routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,50 @@
+/**
+ * set routers
+ */
 
-var index = function(req, res){
-  res.render('index', { title: 'Express' });
+// get function
+var index = function(req, res) {
+  res.render('index', {title: '首页'});
+};
+
+var login = function(req, res) {
+  res.render('login', {title: '登录'});
+};
+
+var reg = function(req, res) {
+  res.render('reg', {title: '注册'});
+};
+
+var post = function(req, res) {
+  res.render('post', {title: '发布'});
+};
+
+// post function
+
+var doLogin = function(req, res) {
+
+};
+
+var doReg = function(req, res) {
+
+};
+
+var doPost = function(req, res) {
+
+};
+
+var doLogout = function(req, res) {
+
 };
 
 module.exports = function(app) {
   app.get('/', index);
+  app.get('/login', login);
+  app.get('/reg', reg);
+  app.get('/post', post);
+
+  app.post('/login', doLogin);
+  app.post('/reg', doReg);
+  app.post('/post', doPost);
+  app.post('/logout', doLogout);
 };
