089ce824c39433fc6055aabe622a1f5c18b432b1
liyachuan <liyachuan@rockontrol.com>
2013-09-25 22:50:11 +0800

    add flash npm

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var path = require('path');
  */
 var MongoStore = require('connect-mongo')(express);
 var settings = require('./settings');
+var flash = require('connect-flash');
 
 var app = express();
 
@@ -21,6 +22,9 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
+app.use(flash());
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
