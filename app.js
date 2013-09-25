ecd2fc53aa1a2b84eb8ef0b3ec337c715d9be56b
liyachuan <liyachuan@rockontrol.com>
2013-09-25 21:52:31 +0800

    add connect-mongo npm

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
+/**
+ * sudo mongod --fork
+ * sudo mongo
+ */
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
+
 var app = express();
 
 // all environments
@@ -18,6 +25,17 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
+app.use(express.cookieParser());
+app.use(express.session({
+  secret: settings.cookieSecret,
+  key: settings.db,
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}, //30 days
+  store: new MongoStore({
+    db: settings.db
+  })
+}));
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
