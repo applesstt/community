0a2cfd6198cac56adbc2130e5b84d0014c76733c
liyachuan <liyachuan@rockontrol.com>
2013-09-26 23:36:08 +0800

    add user model file

---
+++ models/user.js
@@ -0,0 +1,58 @@
+var mongodb = require('./db');
+
+var User = function(user) {
+    this.name = user.name;
+    this.password = user.password;
+    this.email = user.email;
+};
+
+module.exports = User;
+
+User.property.save = function(callback) {
+    var user = {
+        name: this.name,
+        password: this.password,
+        email: this.email
+    };
+
+    mongodb.open(function(err, db) {
+        if(err) {
+            return callback(err);
+        }
+
+        db.collection('users', function(err, collection) {
+            if(err) {
+                mongodb.close();
+                return callback(err);
+            }
+            collection.insert(user, {safe: true}, function(err, user) {
+                mongodb.close();
+                callback(null);
+            })
+        });
+
+    })
+};
+
+User.get = function(name, callback) {
+    mongodb.open(function(err, db) {
+        if(err) {
+            return callback(err);
+        }
+        db.collection('users', function(err, collection) {
+            if(err) {
+                callback(err);
+            }
+            collection.findOne({
+                name: name
+            }, function(err, callback) {
+                mongodb.close();
+                if(user) {
+                    return callback(null, user);
+                }
+                return callback(err);
+            })
+        })
+    })
+};
+
