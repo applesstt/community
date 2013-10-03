e2a26c107b8d60e83133464bec9e6a3b47feb73b
liyachuan <liyachuan@rockontrol.com>
2013-10-03 23:19:29 +0800

    recode get image file on post

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ var checkNotLogin = function(req, res, next) {
 
 // get function
 var index = function(req, res) {
-  Post.get(null, function(err, posts) {
+  Post.getAll(null, function(err, posts) {
     if(err) {
       posts = [];
     }
@@ -131,18 +131,10 @@ var doReg = function(req, res) {
 
 var doPost = function(req, res) {
   var user = req.session.user;
-  var image_name = '';
-  for(var i in req.files) {
-    if(req.files[i].size == 0) {
-      req.unlinkSync(req.files[i].path);
-      console.log('delete empty file');
-    } else {
-      image_name = req.files[i].name;
-      var target_path = './public/upload/images/' + image_name;
-      fs.renameSync(req.files[i].path, target_path);
-      console.log('Success upload image file!');
-      break;
-    }
+  var image_name = req.files.image.name;
+  if(image_name !== '') {
+    var target_path = './public/upload/images' + image_name;
+    fs.renameSync(req.files.image.path, target_path);
   }
   var post = new Post({
     name: user.name,
@@ -179,7 +171,6 @@ module.exports = function(app) {
   app.get('/post', checkLogin);
   app.post('/post', doPost);
 
-  app.get('/post', checkLogin);
   app.get('/logout', logout);
 
   app.use(function(req, res) {
