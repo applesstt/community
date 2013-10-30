2776fe03af4a0e2a8ee3fea71a584f85763cc84b
liyachuan <liyachuan@rockontrol.com>
2013-10-30 15:53:37 +0800

    add wysihtml5 plugin

---
+++ public/javascripts/post.js
@@ -0,0 +1,13 @@
+var editor = new wysihtml5.Editor("wysihtml5-editor", {
+  toolbar:     "wysihtml5-editor-toolbar",
+  stylesheets: ["http://yui.yahooapis.com/2.9.0/build/reset/reset-min.css", "vendor/wysihtml5/css/editor.css"],
+  parserRules: wysihtml5ParserRules
+});
+
+editor.on("load", function() {
+  var composer = editor.composer,
+    h1 = editor.composer.element.querySelector("h1");
+  if (h1) {
+    composer.selection.selectNode(h1);
+  }
+});
