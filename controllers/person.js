c9f9719bd18a739dadc4cefd78683f593ceb8773
liyachuan <liyachuan@rockontrol.com>
2014-04-21 05:33:00 +0800

    Refine person aside style

--- controllers/person.js
+++ controllers/person.js
@@ -1,3 +1,9 @@
 exports.toSet = function(req, res) {
-  res.render('person/set');
+  res.render('person/set', {
+    title: '基本信息',
+    user: req.session.user,
+    env: req.session.env,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
 }
