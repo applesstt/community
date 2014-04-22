fbd8d23442d3cc5f95e4dce9012f3a7103b52460
liyachuan <liyachuan@rockontrol.com>
2014-04-23 06:50:30 +0800

    Post user img and popup dialog for crop

--- public/js/personSet.js
+++ public/js/personSet.js
@@ -1,8 +1,22 @@
 var PersonSet = (function() {
 
   var _initUploadImg = function() {
-    $('.edit-user-img .btn').click(function() {
+    $('.edit-user-img-btn .btn').click(function() {
       $(this).parent().find('input').trigger('click');
+    });
+    $('.edit-user-img-btn input').change(function() {
+      $('#upload-form').ajaxSubmit({
+        url: '/uploadImage',
+        type: 'POST',
+        dataType: 'json',
+        success: function(result) {
+          if(result) {
+            var imagePath200 = 'http://' + location.host + result.base_path + '200_' + result.image;
+            $('#image-200').attr('src', imagePath200);
+            $('#resize-user-img-btn').trigger('click');
+          }
+        }
+      })
     })
   };
 
